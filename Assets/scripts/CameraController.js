6d73081a435efb3c9d8a06546d97facfe542b672
Joe Anzalone <Joe@Shmit.com>
2015-12-26 22:04:56 -0500

    Initial commit

---
+++ Assets/scripts/CameraController.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+public var player : GameObject;
+private var offset : Vector3;
+
+function Start () {
+	offset = transform.position - player.transform.position;
+}
+
+// Runs every frame
+// but guaranteed to run after all items have been processesed in Update()
+function LateUpdate () {
+	transform.position = player.transform.position + offset;
+}
---
+++ Assets/scripts/PlayerController.js
@@ -0,0 +1,49 @@
+﻿#pragma strict
+import UnityEngine.UI;
+
+// Add a thrust force to push an object in its current forward
+// direction (to simulate a rocket motor, say).
+// var thrust: float;
+var speed : float;
+var scoreText : Text;
+var winText : Text;
+
+private var rb : Rigidbody;
+private var score : int = 0;
+
+function updateScoreText () {
+    scoreText.text = 'Score: ' + score.ToString();
+    // winText.text = 'You Win';
+}
+
+function Start () {
+    rb = GetComponent.<Rigidbody>();
+    updateScoreText();
+}
+
+// Before performing any physics calculations
+function FixedUpdate () {
+    var moveHorizontal = Input.GetAxis('Horizontal');
+    var moveVertical = Input.GetAxis('Vertical');
+    // moveHorizontal
+    // moveVertical
+    // movement = new Vector3(moveHorizontal);
+    // rb.AddForce(v3, 0);
+    rb.AddForce(moveHorizontal * speed, 0, moveVertical * speed);
+}
+
+function OnTriggerEnter (other : Collider) {
+    if (other.gameObject.CompareTag('pickup')) {
+        other.gameObject.SetActive(false);
+        score++;
+        updateScoreText();
+        if (score >= 12) {
+            winText.gameObject.SetActive(true);
+        }
+    }
+}
+
+// Before rendering a frame
+function Update () {
+
+}
